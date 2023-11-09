8ad037a7a485fbb6e4d3b8fa70bba9438b6c1ceb
git-pixel22 <workemail.rajat@gmail.com>
2023-11-10 00:57:29 +0530

    Version 1 or 0.0.1 Complete

--- pixelconvert/__init__.py
+++ pixelconvert/__init__.py
@@ -1,1 +1,1 @@
-from .main import convert_png_to_jpeg, convert_jpeg_to_png, convert_pdf_to_docx, main
+from .main import convert_png_to_jpeg, convert_jpeg_to_png, convert_pdf_to_docx, convert_docx_to_pdf, main
--- pixelconvert/main.py
+++ pixelconvert/main.py
@@ -1,7 +1,14 @@
 from PIL import Image
 from sys import argv
 from pdf2docx import parse
-import os
+import subprocess
+
+def convert_docx_to_pdf(inputPath):
+    try:
+        subprocess.run(['libreoffice', '--headless', '--convert-to', 'pdf', '--outdir', './', inputPath], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        # print(f"Conversion successful! {inputPath} converted to output.pdf")
+    except Exception as e:
+        print(f"Conversion failed due to: {e}")
 
 def convert_pdf_to_docx(inputPath):
 
@@ -11,18 +18,18 @@ def convert_pdf_to_docx(inputPath):
     parse(pdf_file, docx_file)
 
 
-def convert_png_to_jpeg(img):
+def convert_png_to_jpeg(inputPath):
     
-    img = Image.open(argv[1])
+    img = Image.open(inputPath)
 
     if img.mode == 'RGBA':
         print("Error: Cannot convert PNG image with transparent background to JPEG.")
     else:
         img.save("output.jpg")
 
-def convert_jpeg_to_png(img):
+def convert_jpeg_to_png(inputPath):
 
-    img = Image.open(argv[1])
+    img = Image.open(inputPath)
 
     img.save("output.png")
 
@@ -32,11 +39,15 @@ def main():
         print("Usage: pixelconvert <image-path> <format-to-convert-to>")
 
     if argv[2] == "jpeg" or argv[2] == "jpg":
-        convert_png_to_jpeg(img)
+        convert_png_to_jpeg(argv[1])
     elif argv[2] == "png":
-        convert_jpeg_to_png(img)
+        convert_jpeg_to_png(argv[1])
     elif argv[2] == "docx":
         convert_pdf_to_docx(argv[1])
+    elif argv[2] == "pdf":
+        convert_docx_to_pdf(argv[1])
+
+
     else:
         print("Invalid format provided.")
 
